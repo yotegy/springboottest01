f1265be4e9d2c489ed3b00de232e010c6ed5a137
Timjoo <timjoo@sk.com>
2020-03-02 12:41:45 +0900

    thymeleaf version

--- src/main/java/home/timjoo/MessageController.java
+++ src/main/java/home/timjoo/MessageController.java
@@ -1,16 +1,17 @@
 package home.timjoo;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 @RequestMapping("/messages")
 public class MessageController {
-    @GetMapping("/welcome")
-    @ResponseBody
-    public String welcome(){
-        return "Hello, Welcome Message server";
+    @GetMapping("/welcome")     
+    public String welcome(Model model){
+        String msg = "Hello, Welcome Message server with thymeleaf";
+        model.addAttribute("message",msg);
+        return "welcome";
     }
 }
