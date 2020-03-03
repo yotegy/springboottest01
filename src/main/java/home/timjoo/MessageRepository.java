91e33be550019fd036b0376cd17138af77fcd09b
Timjoo <timjoo@sk.com>
2020-03-03 10:34:42 +0900

    spring jdbc template

--- src/main/java/home/timjoo/MessageRepository.java
+++ src/main/java/home/timjoo/MessageRepository.java
@@ -11,51 +11,36 @@ import javax.sql.DataSource;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
-import org.springframework.jdbc.datasource.DataSourceUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataAccessException;
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.jdbc.support.GeneratedKeyHolder;
 import org.springframework.stereotype.Component;
 
 @Component
 public class MessageRepository {
     private final static Log log = LogFactory.getLog(MessageRepository.class);
-    
-    private DataSource dataSource;
+    private NamedParameterJdbcTemplate jdbcTemplate;
 
-    public MessageRepository(DataSource dataSource){
-        this.dataSource = dataSource;
-    }
+    @Autowired
+    private void setDataSource(DataSource dataSource){
+        this.jdbcTemplate = new NamedParameterJdbcTemplate(dataSource);
+    };
 
     public Message saveMessage(Message message){
-        Connection conn = DataSourceUtils.getConnection(dataSource);
-        try {
-            String insertSQL = "INSERT INTO messages (`id`,`text`,`created_date`) VALUE (null, ?, ?)";
-            PreparedStatement ps = conn.prepareStatement(insertSQL, Statement.RETURN_GENERATED_KEYS);
-            ps.setString(1,message.getText());
-            ps.setTimestamp(2,new Timestamp(message.getCreatedDate().getTime()));
-            int rowsAffected = ps.executeUpdate();
 
-            if(rowsAffected >0 ){
-                ResultSet result = ps.getGeneratedKeys();
-                if(result.next()){
-                    int id = result.getInt(1);
-                    return new Message(id, message.getText(), message.getCreatedDate());
-                } else {
-                    log.error("Failed to retrieve id, No row in result");
-                    return null;
-                }
-            } else {
-                log.error("Failed to insert message into DB");
-                return null;
-            }
-        }catch (SQLException ex){
+        GeneratedKeyHolder holder = new GeneratedKeyHolder();
+        MapSqlParameterSource params = new MapSqlParameterSource();
+        params.addValue("text", message.getText());
+        params.addValue("created_date", message.getCreatedDate());
+        String insertSQL = "INSERT INTO messages (`id`,`text`,`created_date`) VALUE (null, :text, :created_date)";
+        try{
+            this.jdbcTemplate.update(insertSQL,params,holder);
+        } catch (DataAccessException ex){
             log.error("Failed to save message",ex);
-            try{
-                conn.close();
-            }catch(SQLException e){
-                log.error("Failed to close connection",ex);
-            }
-        } finally {
-            DataSourceUtils.releaseConnection(conn, dataSource);
-        }
-        return null;
+            return null;
+        } 
+        return new Message(holder.getKey().intValue(),message.getText(),message.getCreatedDate());
     }
 }
