3347bc5867082230e06d8fdb89924b255a6428f6
Timjoo <timjoo@sk.com>
2020-03-03 22:51:33 +0900

    first transactinal

--- src/main/java/home/timjoo/AppConfig.java
+++ src/main/java/home/timjoo/AppConfig.java
@@ -7,11 +7,14 @@ import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.orm.hibernate5.HibernateTransactionManager;
 import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 
 @Configuration
 @ComponentScan("home.timjoo")
+@EnableTransactionManagement
 public class AppConfig {
     
     private DataSource dataSource;
@@ -38,6 +41,13 @@ public class AppConfig {
         registration.setUrlPatterns(Arrays.asList("/messages/*"));
 
         return registration;
+    }
+
+    @Bean
+    public HibernateTransactionManager transactionManager(){
+        HibernateTransactionManager transactionManager = new HibernateTransactionManager();
+        transactionManager.setSessionFactory(sessionFactory().getObject());
+        return transactionManager;
     }
 
 }
--- src/main/java/home/timjoo/MessageService.java
+++ src/main/java/home/timjoo/MessageService.java
@@ -1,16 +1,30 @@
 package home.timjoo;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
 
 @Component
 public class MessageService{
+
+    private static final Log log = LogFactory.getLog(MessageService.class);
 
     @Autowired
     private MessageRepository repository;
 
     @SecurityCheck
+    @Transactional(noRollbackFor = { UnsupportedOperationException.class })
     public Message save(String msg){
-        return repository.saveMessage(new Message(msg));
+        Message message = repository.saveMessage(new Message(msg));
+        log.debug("New message[id="+ message.getId()+"] saved");
+        updateStatistics();
+        return message;
+    }
+
+    private void updateStatistics(){
+        throw new UnsupportedOperationException("This method is not implemented yet");
+
     }
 }
